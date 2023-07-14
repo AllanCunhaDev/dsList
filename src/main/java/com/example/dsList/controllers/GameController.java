30534e94308c935e132c7c4d1470ca01adeb39ed
Allan <allancunha20@hotmail.com>
2023-07-14 11:16:37 -0300

    Belonging primary key

--- src/main/java/com/example/dsList/controllers/GameController.java
+++ src/main/java/com/example/dsList/controllers/GameController.java
@@ -3,6 +3,7 @@ package com.example.dsList.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,12 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping()
     public List<GameMinDTO> findAll(){
 
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
+
+    
 }
